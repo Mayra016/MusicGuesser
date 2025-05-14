740b52af86a60bf32526630a046f49f6a863a002
Mayra016 <mayrazucaro@gmail.com>
2025-05-14 18:18:30 -0300

    feat add game logic and template

--- src/app/Game/play-component/play-component.component.ts
+++ src/app/Game/play-component/play-component.component.ts
@@ -7,6 +7,7 @@ import textDE from '../../translations/textDE';
 import textES from '../../translations/textES';
 import textPT from '../../translations/textPT';
 import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 
 @Component({
@@ -17,7 +18,7 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./play-component.component.css']
 })
 export class PlayComponentComponent {
-  constructor(private cookies:CookieService, private route:ActivatedRoute){
+  constructor(private cookies:CookieService, private route:ActivatedRoute, private httpClient:HttpClient){
     this.translate;
     this.generateLetters;
   }
@@ -28,12 +29,27 @@ export class PlayComponentComponent {
   userWord: string = "";
   music: string = "";
   lyrics: string = "";
-  
+  autocompleteApiURL: string = "api.genius.com/search?q=";
+  lyricsApiUrl: string = "https://api.lyrics.ovh/v1";
+  autocompleteResult = {};
+  songs: string[] = [];
+  won: boolean = false;
+  lost: boolean = false;
+  score: number = 0;
+  lifes: number = 3;
+  Array = Array;
+
 
   // translations
   titleTxt: string = textEN.gameTitle;
   wordTxt: string = textEN.wordTxt;
   sendTxt: string = textEN.send;
+  scoreTxt: string = "Score:";
+  winTxt: string = textEN.win;
+  nextTxt: string = textEN.next;
+  lostTxt: string = textEN.lostTitle;
+  lostScore: string = textEN.scoreText;
+  playAgainTxt: string = textEN.playAgain;
   
 
   ngOnInit(): void {
@@ -46,6 +62,13 @@ export class PlayComponentComponent {
       this.titleTxt = textES.gameTitle;
       this.wordTxt = textES.wordTxt;
       this.sendTxt = textES.send;
+      this.scoreTxt = textES.score;
+      this.winTxt = textES.win;
+      this.nextTxt = textES.next;
+      this.lostTxt = textES.lostTitle;
+      this.lostScore = textES.scoreText;
+      this.playAgainTxt = textES.playAgain;
+
     } else if (this.language == 'DE' || this.language == 'German') {
       this.titleTxt = textDE.gameTitle;
       this.wordTxt = textDE.wordTxt;
@@ -54,6 +77,7 @@ export class PlayComponentComponent {
     } else if (this.language == 'PT' || this.language == 'Portuguese') {
       this.titleTxt = textPT.gameTitle;
       this.wordTxt = textPT.wordTxt;
+      this.sendTxt = textPT.send;
     } else {
 
     }
@@ -65,10 +89,66 @@ export class PlayComponentComponent {
     this.letters += this.availableLetters.charAt(Math.floor((Math.random() * this.availableLetters.length - 4) + 1));
   }
 
-  // check user input
-
+  selectedSong(selectedSong:string) {
+    this.music = selectedSong;
+    let userInput = selectedSong.split(" by ");
+    // fetch lyrics api
+    this.lyrics = String(this.httpClient.get(this.lyricsApiUrl + "/" + selectedSong[1] + "/" + selectedSong[0]));
+  }
 
   autocomplete(music: string) {
-    // fetch api
+    // fetch autocomplete Genius Api
+    const headers: HttpHeaders = new HttpHeaders();
+    headers.set("Authorization:", "Bearer -riMsB6htPjCj--rQmpY4EyFTwRViRwcOgmnaBhUfOENWwrZVbBGP5sFB1RH6xcE");
+    headers.set('Content-Type', 'application/json');
+    headers.set('Accept', 'application/json');
+    this.httpClient.get<any>(this.autocompleteApiURL + music, { headers }).subscribe({
+      next: (response) => {
+        this.songs = response?.response?.hits?.result?.map(
+          (item: any) => item.full_title
+        ) || [];
+        
+      },
+      error: (err) => console.error('Error:', err)
+    });
+  }
+
+  checkUserInput() {
+    this.userWord = this.userWord.toUpperCase();
+
+    if (this.userWord.includes(this.letters.charAt(0)) &&
+       this.userWord.includes(this.letters.charAt(1)) &&
+       this.lyrics.includes(this.userWord)) {
+      this.won = true;
+      this.score += 10;
+    } else {
+      this.lifes = this.lifes - 1;
+      if (this.lifes == 0) {
+        this.lost = true;
+        this.score = 0;    
+      }   
+    }
+
+  }
+
+  nextLevel() {
+    this.resetLevel;
+    this.won = false;
+  }
+
+  playAgain() {
+    this.resetLevel;
+    this.lost = false;
+    this.score = 0;
+    this.lifes = 3
+  }
+
+  resetLevel() {
+    this.userWord = "";
+    this.music = "";
+    this.lyrics = "";
+    this.songs = [];
+    this.autocompleteResult = {};
+    this.generateLetters;
   }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,8 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { CookieService } from 'ngx-cookie-service';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), CookieService]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), CookieService, provideHttpClient()]
 };
