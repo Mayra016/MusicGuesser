dded07f4314b1d8fd63cc6bac3dd7f169cbb6b38
Mayra016 <mayrazucaro@gmail.com>
2025-06-05 17:53:20 -0300

    import animations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,8 +4,10 @@ import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { CookieService } from 'ngx-cookie-service';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
 
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), CookieService, provideHttpClient()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), CookieService, provideHttpClient(), provideAnimations()]
 };
